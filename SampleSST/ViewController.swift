aeada212b62853dc20fb94de31f7920b2400fafe
Myron Wells <myronjwells@gmail.com>
2018-03-09 13:05:06 -0600

    Initial Commit

---
+++ SampleSST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSST
+//
+//  Created by Myron Wells on 3/9/18.
+//  Copyright © 2018 Myron Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
